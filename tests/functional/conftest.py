fc040050903c74cb12109789d89f8683b1dcd9c1
Artemis875 <asgumeniuk@edu.hse.ru>
2025-08-07 20:04:44 +0300

    Update conftest.py

--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -1,19 +1,21 @@
+import pytest_asyncio
 import pytest
 from httpx import AsyncClient
 from src.main import app
 from src.database import Base, async_session
 
-@pytest.fixture
+
+@pytest_asyncio.fixture
 async def client():
     async with AsyncClient(app=app, base_url="http://test") as client:
         yield client
 
-@pytest.fixture
+@pytest_asyncio.fixture
 async def test_client():
     async with AsyncClient(app=app, base_url="http://test") as client:
         yield client
 
-@pytest.fixture(autouse=True)
+@pytest_asyncio.fixture(autouse=True)
 async def prepare_db():
     async with async_session() as session:
         await session.run_sync(Base.metadata.create_all)
