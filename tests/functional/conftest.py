6d151b2955a31b09b66e8c955cdb12ae5d6ae274
Artemis875 <asgumeniuk@edu.hse.ru>
2025-08-07 17:11:11 +0300

    Update conftest.py

--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -1,7 +1,12 @@
 import pytest
 from httpx import AsyncClient
 from src.main import app
-from sqlalchemy.ext.asyncio import AsyncSession
+from src.database import Base, async_session
+
+@pytest.fixture
+async def client():
+    async with AsyncClient(app=app, base_url="http://test") as client:
+        yield client
 
 @pytest.fixture
 async def test_client():
@@ -10,7 +15,6 @@ async def test_client():
 
 @pytest.fixture(autouse=True)
 async def prepare_db():
-    from src.database import async_session, Base
     async with async_session() as session:
         await session.run_sync(Base.metadata.create_all)
         yield
